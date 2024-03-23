4119d9e9d730346be84f576a783827286f25a458
nwfl <111260025+kanaee-cloud@users.noreply.github.com>
2024-03-23 13:13:23 +0700

    Add files via upload

---
+++ c# 3/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("JAWABAN NO 1");
+Console.WriteLine("============");
+Console.WriteLine("Semangat Pagi");
+Console.WriteLine("Muhammad Arsal Nawfal Ali");
+
+Console.WriteLine("JAWABAN NO 2");
+Console.WriteLine("============");
+Console.WriteLine("silahkan masukkan bilangan: ");
+
+Console.WriteLine("Bilangan Pertama:");
+double angka1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Bilangan Kedua:");
+double angka2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Bilangan Ketiga:");
+double angka3 = double.Parse(Console.ReadLine());
+
+double total = angka1 + angka2 + angka3;
+Console.WriteLine("Hasil dari inputan tadi adalah : " + total);
+
+
+Console.WriteLine("JAWABAN NO 3");
+Console.WriteLine("============");
+
+Console.WriteLine("3 Bagian A");
+double perkalian = 3.5 * 10;
+Console.WriteLine(-1 + perkalian);
+
+Console.WriteLine("3 Bagian B");
+double perkalian2 = 4.5 * 2;
+Console.WriteLine(20 - perkalian2 / 11);
+
+Console.WriteLine("3 Bagian C");
+double perkalian3 = 4.5 * 2;
+double modulus = 7 % 3.5;
+Console.WriteLine(2 + 15 / perkalian3 - modulus);
+
+Console.WriteLine("3 Bagian D");
+double penjumlahan = 32.5 + 8.5;
+Console.WriteLine(penjumlahan % 10.5);
+
+
+
+
+
+
+
+
+
+
---
+++ tes c# 2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static void Main(string[] args){
+        Console.WriteLine("Nama Barang :");
+        string namaBarang = Console.ReadLine();
+
+        Console.WriteLine("Harga Barang");
+        int hargaBarang = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Kuantitas Barang");
+        int kuantitiBarang = Convert.ToInt32(Console.ReadLine());
+
+        int totalHarga = hargaBarang * kuantitiBarang;
+
+         Console.WriteLine("RRTOTO");
+         Console.WriteLine("===========");
+
+         Console.WriteLine("Nama Barang      : " + namaBarang);
+         Console.WriteLine("Harga Barang     : " + hargaBarang);
+         Console.WriteLine("Kuantitas Barang : " + kuantitiBarang);
+         Console.WriteLine("Total            : " + totalHarga);
+         Console.WriteLine("===========");
+
+
+        if( totalHarga >= 100000){
+            int discount = totalHarga * 2 / 100;
+            int potongHarga = totalHarga - discount;
+            Console.WriteLine("Selamat anda mendapat potongan harga menjadi :" + potongHarga);
+        } else{
+            Console.WriteLine("Terimakasih sudah berbelanja disini");
+        }
+
+    }
+}
